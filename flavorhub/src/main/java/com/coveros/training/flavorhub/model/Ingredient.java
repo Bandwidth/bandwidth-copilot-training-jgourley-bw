a45bf1f2d69bfcf236ba3278c38b199dce833f54
Josiah Gourley <jgourley@bandwidth.com>
2026-02-18 09:56:12 -0500

    this is just explorign vscode git integration

--- flavorhub/src/main/java/com/coveros/training/flavorhub/model/Ingredient.java
+++ flavorhub/src/main/java/com/coveros/training/flavorhub/model/Ingredient.java
@@ -35,4 +35,14 @@ public class Ingredient {
         this.category = category;
         this.unit = unit;
     }
+
+
+    //New sub-DTO that represents an ingredient's molecular composition
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor 
+    public static class MolecularComposition {
+        private String chemicalFormula; // e.g., "C6H12O6" for glucose
+        private double molecularWeight; // in g/mol
+    }
 }
